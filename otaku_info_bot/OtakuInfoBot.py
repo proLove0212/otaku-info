edda2d2ca4e036f7f4f882b0aa539422819a4cd0
PROLOVE <proLove0212@gmail.com>
2019-09-24 22:29:11 +0200

    Fix string call on notification object

--- otaku_info_bot/OtakuInfoBot.py
+++ otaku_info_bot/OtakuInfoBot.py
@@ -291,7 +291,7 @@ class OtakuInfoBot(Bot):
 
             media_type = notification.entry.media_type
             if notification.diff > 0:
-                self.logger.debug("Notification due: " + notification)
+                self.logger.debug("Notification due: " + str(notification))
                 due[address_id][media_type].append(notification)
             elif use_user_progress and notification.user_diff > 0:
                 due[address_id][media_type].append(notification)
