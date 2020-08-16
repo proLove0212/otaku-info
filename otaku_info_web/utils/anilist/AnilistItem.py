baccfab363992250181fa45a54bbec575de545b1
PROLOVE <proLove0212@gmail.com>
2020-08-16 19:07:11 +0200

    Update project with new ways to deploy in docker

--- otaku_info_web/utils/anilist/AnilistItem.py
+++ otaku_info_web/utils/anilist/AnilistItem.py
@@ -1,20 +1,20 @@
 """LICENSE
 Copyright 2020 Hermann Krumrey <hermann@krumreyh.com>
 
-This file is part of otaku-info-web.
+This file is part of otaku-info.
 
-otaku-info-web is free software: you can redistribute it and/or modify
+otaku-info is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-otaku-info-web is distributed in the hope that it will be useful,
+otaku-info is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with otaku-info-web.  If not, see <http://www.gnu.org/licenses/>.
+along with otaku-info.  If not, see <http://www.gnu.org/licenses/>.
 LICENSE"""
 
 from typing import Optional, Dict, Any, Tuple
